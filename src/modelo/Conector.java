59b2e25cf15ac91204ac6d3a4cf1f0f9714a206a
Ander <Ander@DESKTOP-C72D24L>
2020-05-07 12:11:05 +0200

    Poryecto final.

---
+++ src/modelo/Conector.java
@@ -0,0 +1,15 @@
+package modelo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Conector {
+
+	protected Connection conexion;
+	
+	protected Conector() {
+		try {}
+	}
+	
+	
+	
+}
